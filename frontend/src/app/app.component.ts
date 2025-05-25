3b659ae33a400a0dd554305b5d8ca30a111a7a55
Lucas Lombardo <lucasalombardo@gmail.com>
2025-05-24 23:59:46 -0400

    [FE] Adds home and login pages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
